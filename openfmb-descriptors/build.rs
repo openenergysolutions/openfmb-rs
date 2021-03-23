0dae374c0be5bfb28bb72496f41dbbb7254b46b8
Tom Burdick <tom@openenergysolutionsinc.com>
2021-03-23 11:47:10 -0500

    OpenFMB Descriptors should compile almost anywhere

--- openfmb-descriptors/build.rs
+++ openfmb-descriptors/build.rs
@@ -1,7 +1,85 @@
 use std::process::Command;
 use std::{env, fs};
+use std::path::PathBuf;
 use std::error::Error;
 use std::ffi::OsStr;
+
+
+/// Returns the path to the location of the bundled Protobuf artifacts.
+fn bundle_path() -> PathBuf {
+    env::current_dir()
+        .unwrap()
+        .join("third-party")
+        .join("protobuf")
+}
+
+/// Returns the path to the `protoc` pointed to by the `PROTOC` environment variable, if it is set.
+fn env_protoc() -> Option<PathBuf> {
+    let protoc = match env::var_os("PROTOC") {
+        Some(path) => PathBuf::from(path),
+        None => return None,
+    };
+
+    Some(protoc)
+}
+
+/// Returns the path to the bundled `protoc`, if it is available for the host platform.
+#[cfg(not(target_env = "musl"))]
+fn bundled_protoc() -> Option<PathBuf> {
+    let protoc_bin_name = match (env::consts::OS, env::consts::ARCH) {
+        ("linux", "x86") => "protoc-linux-x86_32",
+        ("linux", "x86_64") => "protoc-linux-x86_64",
+        ("linux", "aarch64") => "protoc-linux-aarch_64",
+        ("macos", "x86_64") => "protoc-osx-x86_64",
+        ("macos", "aarch64") => "protoc-osx-x86_64", // will be translated to aarch64 by Rosetta
+        ("windows", _) => "protoc-win32.exe",
+        _ => return None,
+    };
+
+    Some(bundle_path().join(protoc_bin_name))
+}
+
+/// `musl` build hosts do not have a bundled `protoc`.
+///
+/// Note: this checks the target of the `prost-build` build.rs, which is ultimately the host architecture.
+#[cfg(target_env = "musl")]
+fn bundled_protoc() -> Option<PathBuf> {
+    None
+}
+
+/// Returns the path to the `protoc` included on the `PATH`, if it exists.
+fn path_protoc() -> Option<PathBuf> {
+    which::which("protoc").ok()
+}
+
+/// Returns the path to the Protobuf include directory pointed to by the `PROTOC_INCLUDE`
+/// environment variable, if it is set.
+fn env_protoc_include() -> Option<PathBuf> {
+    let protoc_include = match env::var_os("PROTOC_INCLUDE") {
+        Some(path) => PathBuf::from(path),
+        None => return None,
+    };
+
+    if !protoc_include.exists() {
+        panic!(
+            "PROTOC_INCLUDE environment variable points to non-existent directory ({:?})",
+            protoc_include
+        );
+    }
+    if !protoc_include.is_dir() {
+        panic!(
+            "PROTOC_INCLUDE environment variable points to a non-directory file ({:?})",
+            protoc_include
+        );
+    }
+
+    Some(protoc_include)
+}
+
+/// Returns the path to the bundled Protobuf include directory.
+fn bundled_protoc_include() -> PathBuf {
+    bundle_path().join("include")
+}
 
 fn proto_paths() -> Result<Vec<String>, Box<dyn Error>> {
     let mut proto_paths: Vec<String> = Vec::new();
@@ -33,17 +111,36 @@ fn proto_paths() -> Result<Vec<String>, Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+let protoc = env_protoc()
+        .or_else(bundled_protoc)
+        .or_else(path_protoc)
+        .expect(
+            "Failed to find the protoc binary. The PROTOC environment variable is not set, \
+             there is no bundled protoc for this platform, and protoc is not in the PATH",
+        );
+
+    let protoc_include = env_protoc_include().unwrap_or_else(bundled_protoc_include);
+
+    println!("cargo:rustc-env=PROTOC={}", protoc.display());
+    println!(
+        "cargo:rustc-env=PROTOC_INCLUDE={}",
+        protoc_include.display()
+    );
+    println!("cargo:rerun-if-env-changed=PROTOC");
+    println!("cargo:rerun-if-env-changed=PROTOC_INCLUDE");
+
+
     println!("looking up paths");
     let paths = proto_paths()?;
     println!("proto paths {:?}", paths);
-    let mut protoc = Command::new("protoc");
-    protoc.current_dir("proto");
-    protoc.arg("-oopenfmb_descriptors.pb");
+    let mut protoc_cmd = Command::new(protoc);
+    protoc_cmd.current_dir("proto");
+    protoc_cmd.arg("-oopenfmb_descriptors.pb");
     for proto_path in paths {
-        protoc.arg(proto_path.clone());
+        protoc_cmd.arg(proto_path.clone());
     }
 
-    let output = protoc.output()?;
+    let output = protoc_cmd.output()?;
     println!("output of command {:?}", output);
 
 
