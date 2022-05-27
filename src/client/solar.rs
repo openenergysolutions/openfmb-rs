e92cb4803a8faf4f8928a97c62268d3a90206053
Timothy <tim@openenergysolutionsinc.com>
2022-05-27 12:03:08 -0700

    Implement set_WNetMag_schedule()

--- src/client/solar.rs
+++ src/client/solar.rs
@@ -2,15 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::prelude::*;
-use openfmb_messages::{
-    solarmodule::{
-        SolarControlProfile, SolarEventProfile, SolarReadingProfile, SolarStatusProfile,
-    },
-    Module, Profile,
-};
-
+use openfmb_messages::{commonmodule::*, solarmodule::*, Module, Profile};
+use openfmb_messages_ext::solar::SolarControlExt;
 
 use uuid::Uuid;
+
+use std::time;
 
 /// Provide functionality to publish and accept messages a switch device
 /// needs.
@@ -57,6 +54,10 @@ where
         }
     }
 
+    fn mrid_as_string(&self) -> String {
+        format!("{}", self.mrid.to_hyphenated())
+    }
+
     /// Subscribe to Solar status messages
     pub async fn status(&mut self) -> SubscribeResult<SolarStatusProfile> {
         Ok(self.bus.subscribe(self.status_topic.iter()).await?)
@@ -75,5 +76,41 @@ where
     /// Publish Solar control messages
     pub async fn control(&mut self, msg: SolarControlProfile) -> PublishResult<()> {
         self.bus.publish(self.control_topic.iter(), msg).await
+    }
+
+    /// Publish a Solar control message with a specified number of WNetMag schedule values
+    pub async fn set_WNetMag_schedule(&mut self, sch_pts: usize, value: f64) -> PublishResult<()> {
+        let mut msg = SolarControlProfile::build_control_profile(
+            &self.mrid_as_string(),
+            value,
+            time::SystemTime::now(),
+            0,
+        );
+        for _ in 0..sch_pts {
+            let (seconds, nanoseconds) =
+                match time::SystemTime::now().duration_since(time::SystemTime::UNIX_EPOCH) {
+                    Ok(time) => (time.as_secs(), time.subsec_nanos()),
+                    Err(_) => panic!("SystemTime before UNIX_EPOCH!"),
+                };
+            let ctrl_timestamp = ControlTimestamp {
+                seconds,
+                nanoseconds,
+            };
+            let sch_pt = SchedulePoint {
+                schedule_parameter: vec![EngScheduleParameter {
+                    schedule_parameter_type: ScheduleParameterKind::WNetMag.into(),
+                    value,
+                }],
+                start_time: Some(ctrl_timestamp),
+            };
+            msg.solar_control_mut()
+                .solar_control_fscc_mut()
+                .control_fscc_mut()
+                .control_schedule_fsch_mut()
+                .val_acsg_mut()
+                .sch_pts_mut()
+                .push(sch_pt);
+        }
+        self.control(msg).await
     }
 }
