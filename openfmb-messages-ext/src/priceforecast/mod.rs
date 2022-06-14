7cb7780340433969cf7080d1ba583f2fcaf7175c
Timothy <tim@openenergysolutionsinc.com>
2022-06-14 08:13:23 -0700

    Remove forecasting artifacts and lock file

--- openfmb-messages-ext/src/priceforecast/mod.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Open Energy Solutions Inc
-//
-// SPDX-License-Identifier: Apache-2.0
-
-mod forecast;
-
-pub use forecast::*;
--- openfmb-messages/src/solarforecastmodule.rs
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2021 Open Energy Solutions Inc
-//
-// SPDX-License-Identifier: Apache-2.0
-
-include!(concat!(env!("OUT_DIR"), "/solarforecastmodule.rs"));
