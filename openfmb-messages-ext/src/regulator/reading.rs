8c481703a2e60e540574df5e50d2f346c125d0dd
Timothy Maloney <timothy.maloney@openenergysolutions.com>
2023-11-10 11:28:03 -0800

    Remove redundant trait
    Signed-off-by: Timothy Maloney <timothy.maloney@openenergysolutions.com>

--- openfmb-messages-ext/src/regulator/reading.rs
+++ openfmb-messages-ext/src/regulator/reading.rs
@@ -9,7 +9,7 @@ use uuid::Uuid;
 use openfmb_messages::{commonmodule::*, *};
 use regulatormodule::RegulatorReadingProfile;
 
-use crate::{error::*, OpenFMBExt, OpenFMBExtReading, ReadingProfileExt, Side};
+use crate::{error::*, OpenFMBExt, OpenFMBExtReading, ReadingProfileExt};
 
 impl OpenFMBExtReading for RegulatorReadingProfile {
     fn reading_message_info(&self) -> OpenFMBResult<&ReadingMessageInfo> {
@@ -67,115 +67,6 @@ impl OpenFMBExt for RegulatorReadingProfile {
             .name
             .clone()
             .context(NoName)?)
-    }
-}
-
-pub trait RegulatorReadingExt {
-    fn w_net(&self, side: Side) -> OpenFMBResult<f64>;
-    fn q_net(&self, side: Side) -> OpenFMBResult<f64>;
-    fn v_net(&self, side: Side) -> OpenFMBResult<f64>;
-    fn a_net(&self, side: Side) -> OpenFMBResult<f64>;
-}
-
-impl RegulatorReadingExt for RegulatorReadingProfile {
-    fn w_net(&self, side: Side) -> OpenFMBResult<f64> {
-        let index = side as usize;
-        if !self.regulator_reading.is_empty() && self.regulator_reading.len() > index {
-            return Ok(self
-                .regulator_reading
-                .get(index)
-                .as_ref()
-                .context(NoRegulatorReading)?
-                .reading_mmxu
-                .as_ref()
-                .context(NoReadingMmxu)?
-                .w
-                .as_ref()
-                .context(NoW)?
-                .net
-                .as_ref()
-                .context(NoNet)?
-                .c_val
-                .as_ref()
-                .context(NoCVal)?
-                .mag);
-        }
-        Err(OpenFMBError::NoRegulatorReading)
-    }
-
-    fn q_net(&self, side: Side) -> OpenFMBResult<f64> {
-        let index = side as usize;
-        if !self.regulator_reading.is_empty() && self.regulator_reading.len() > index {
-            return Ok(self
-                .regulator_reading
-                .get(index)
-                .as_ref()
-                .context(NoRegulatorReading)?
-                .reading_mmxu
-                .as_ref()
-                .context(NoReadingMmxu)?
-                .v_ar
-                .as_ref()
-                .context(NoW)?
-                .net
-                .as_ref()
-                .context(NoNet)?
-                .c_val
-                .as_ref()
-                .context(NoCVal)?
-                .mag);
-        }
-        Err(OpenFMBError::NoRegulatorReading)
-    }
-
-    fn v_net(&self, side: Side) -> OpenFMBResult<f64> {
-        let index = side as usize;
-        if !self.regulator_reading.is_empty() && self.regulator_reading.len() > index {
-            return Ok(self
-                .regulator_reading
-                .first()
-                .as_ref()
-                .context(NoRegulatorReading)?
-                .reading_mmxu
-                .as_ref()
-                .context(NoReadingMmxu)?
-                .ph_v
-                .as_ref()
-                .context(NoValue)?
-                .net
-                .as_ref()
-                .context(NoNet)?
-                .c_val
-                .as_ref()
-                .context(NoCVal)?
-                .mag);
-        }
-        Err(OpenFMBError::NoRegulatorReading)
-    }
-
-    fn a_net(&self, side: Side) -> OpenFMBResult<f64> {
-        let index = side as usize;
-        if !self.regulator_reading.is_empty() && self.regulator_reading.len() > index {
-            return Ok(self
-                .regulator_reading
-                .first()
-                .as_ref()
-                .context(NoRegulatorReading)?
-                .reading_mmxu
-                .as_ref()
-                .context(NoReadingMmxu)?
-                .a
-                .as_ref()
-                .context(NoValue)?
-                .net
-                .as_ref()
-                .context(NoNet)?
-                .c_val
-                .as_ref()
-                .context(NoCVal)?
-                .mag);
-        }
-        Err(OpenFMBError::NoRegulatorReading)
     }
 }
 
