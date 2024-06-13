1a65cb7cd21f8d59404faf3727f380a80a03298f
Cory Nguyen <corynguyenfl@gmail.com>
2024-06-13 13:52:43 -0400

    extended with env module

---
+++ openfmb-messages-ext/src/environment/reading.rs
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2021 Open Energy Solutions Inc
+//
+// SPDX-License-Identifier: Apache-2.0
+
+use std::str::FromStr;
+
+use environmentmodule::EnvironmentReadingProfile;
+use openfmb_messages::{
+    commonmodule::{MessageInfo, ReadingMessageInfo},
+    *,
+};
+use snafu::{OptionExt, ResultExt};
+use uuid::Uuid;
+
+use crate::{error::*, OpenFMBExt, OpenFMBExtReading, ReadingProfileExt};
+
+impl OpenFMBExt for EnvironmentReadingProfile {
+    fn device_state(&self) -> OpenFMBResult<String> {
+        Ok("".to_string())
+    }
+
+    fn message_info(&self) -> OpenFMBResult<&MessageInfo> {
+        Ok(self
+            .reading_message_info
+            .as_ref()
+            .context(NoReadingMessageInfo)?
+            .message_info
+            .as_ref()
+            .context(NoMessageInfo)?)
+    }
+
+    fn message_type(&self) -> OpenFMBResult<String> {
+        Ok("EnvironmentReadingProfile".to_string())
+    }
+
+    fn device_mrid(&self) -> OpenFMBResult<Uuid> {
+        Ok(Uuid::from_str(
+            &self
+                .sensor
+                .as_ref()
+                .context(NoMeter)?
+                .conducting_equipment
+                .as_ref()
+                .context(NoConductingEquipment)?
+                .m_rid,
+        )
+        .context(UuidError)?)
+    }
+
+    fn device_name(&self) -> OpenFMBResult<String> {
+        Ok(self
+            .sensor
+            .as_ref()
+            .context(NoMeter)?
+            .conducting_equipment
+            .as_ref()
+            .context(NoConductingEquipment)?
+            .named_object
+            .as_ref()
+            .context(NoNamedObject)?
+            .name
+            .clone()
+            .context(NoName)?)
+    }
+}
+
+impl OpenFMBExtReading for EnvironmentReadingProfile {
+    fn reading_message_info(&self) -> OpenFMBResult<&ReadingMessageInfo> {
+        Ok(self
+            .reading_message_info
+            .as_ref()
+            .context(NoStatusMessageInfo)?)
+    }
+}
